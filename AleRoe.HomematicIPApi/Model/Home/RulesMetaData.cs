3f97ee74004cafdf4f5e930d8ffe3a1ceea1f53d
Alexander Röthinger <alexander1967muc@hotmail.com>
2022-01-22 14:13:56 +0100

    Model-Fixes to Latest * Added Property: LastExecutionTimestamp * Added Property: ExecutionCounterOfDay * Added Property: SpecialInterestCategories Set all properties as private

--- AleRoe.HomematicIPApi/Model/Home/RulesMetaData.cs
+++ AleRoe.HomematicIPApi/Model/Home/RulesMetaData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using AleRoe.HomematicIpApi.Json;
 using Newtonsoft.Json;
 
 namespace AleRoe.HomematicIpApi.Model.Home
@@ -7,21 +8,32 @@ namespace AleRoe.HomematicIpApi.Model.Home
     public class RuleMetaData
     {
         [JsonProperty("id")]
-        public string Id { get; set; }
+        public string Id { get; private set; }
 
         [JsonProperty("homeId")]
-        public Guid HomeId { get; set; }
+        public Guid HomeId { get; private set; }
 
         [JsonProperty("label")]
-        public string Label { get; set; }
+        public string Label { get; private set; }
 
         [JsonProperty("type")]
-        public AutomationRuleType Type { get; set; }
+        public AutomationRuleType Type { get; private set; }
 
         [JsonProperty("active")]
-        public bool Active { get; set; }
+        public bool Active { get; private set; }
 
         [JsonProperty("ruleErrorCategories")]
-        public List<object> RuleErrorCategories { get; set; }
+        public List<object> RuleErrorCategories { get; private set; }
+
+        [JsonProperty("lastExecutionTimestamp", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonConverter(typeof(UnixTimestampConverter))]
+        public DateTime LastExecutionTimestamp { get; private set; }
+
+        [JsonProperty("executionCounterOfDay")]
+        public int ExecutionCounterOfDay { get; private set; }
+
+        [JsonProperty("specialInterestCategories")]
+        public object SpecialInterestCategories { get; private set; }
+
     }
 }
