84e104d19039e34b5b0b80935b793183c39f9f7b
Alexander Röthinger <alexander1967muc@hotmail.com>
2022-01-22 14:24:16 +0100

    Fixed Model to Latest * Added Property PinChangeTimestamp * Added Property PinChangeClientLabel Set all properties to private

--- AleRoe.HomematicIPApi/Model/Home/Home.cs
+++ AleRoe.HomematicIPApi/Model/Home/Home.cs
@@ -8,96 +8,96 @@ namespace AleRoe.HomematicIpApi.Model.Home
     public class Home
     {
         [JsonProperty("weather")] 
-        public Weather Weather { get; set; }
+        public Weather Weather { get; private set; }
 
         [JsonProperty("metaGroups")] 
-        public List<Guid> MetaGroups { get; set; }
+        public List<Guid> MetaGroups { get; private set; }
 
         [JsonProperty("clients")] 
-        public List<Guid> Clients { get; set; }
+        public List<Guid> Clients { get; private set; }
 
         [JsonProperty("connected")] 
-        public bool Connected { get; set; }
+        public bool Connected { get; private set; }
 
         [JsonProperty("currentAPVersion")] 
-        public string CurrentAPVersion { get; set; }
+        public string CurrentAPVersion { get; private set; }
 
         [JsonProperty("availableAPVersion", NullValueHandling= NullValueHandling.Include)] 
-        public string AvailableAPVersion { get; set; }
+        public string AvailableAPVersion { get; private set; }
 
         [JsonProperty("timeZoneId")] 
-        public string TimeZoneId { get; set; }
+        public string TimeZoneId { get; private set; }
 
         [JsonProperty("location")] 
-        public Location Location { get; set; }
+        public Location Location { get; private set; }
 
         [JsonProperty("pinAssigned")] 
-        public bool PinAssigned { get; set; }
+        public bool PinAssigned { get; private set; }
 
         [JsonProperty("isLiveUpdateSupported")]
-        public bool? IsLiveUpdateSupported { get; set; }
+        public bool? IsLiveUpdateSupported { get; private set; }
 
         [JsonProperty("dutyCycle")] 
-        public double? DutyCycle { get; set; }
+        public double? DutyCycle { get; private set; }
 
         [JsonProperty("carrierSense", NullValueHandling = NullValueHandling.Include)] 
-        public double? CarrierSense { get; set; }
+        public double? CarrierSense { get; private set; }
 
         [JsonProperty("updateState")] 
-        public LiveUpdateState UpdateState { get; set; }
+        public LiveUpdateState UpdateState { get; private set; }
 
         [JsonProperty("powerMeterUnitPrice")] 
-        public double PowerMeterUnitPrice { get; set; }
+        public double PowerMeterUnitPrice { get; private set; }
 
         [JsonProperty("powerMeterCurrency")] 
-        public string PowerMeterCurrency { get; set; }
+        public string PowerMeterCurrency { get; private set; }
 
         [JsonProperty("deviceUpdateStrategy")] 
-        public DeviceUpdateStrategy DeviceUpdateStrategy { get; set; }
+        public DeviceUpdateStrategy DeviceUpdateStrategy { get; private set; }
 
         [JsonProperty("lastReadyForUpdateTimestamp")]
         [JsonConverter(typeof(UnixTimestampConverter))]
-        public DateTime LastReadyForUpdateTimestamp { get; set; }
+        public DateTime LastReadyForUpdateTimestamp { get; private set; }
 
         [JsonProperty("functionalHomes")] 
-        public FunctionalHomes FunctionalHomes { get; set; }
+        public FunctionalHomes FunctionalHomes { get; private set; }
 
         [JsonProperty("inboxGroup")] 
-        public Guid InboxGroup { get; set; }
+        public Guid InboxGroup { get; private set; }
 
         [JsonProperty("apExchangeClientId", NullValueHandling = NullValueHandling.Include)] 
-        public object ApExchangeClientId { get; set; }
+        public object ApExchangeClientId { get; private set; }
 
         [JsonProperty("apExchangeState")] 
-        public ApExchangeState ApExchangeState { get; set; }
+        public ApExchangeState ApExchangeState { get; private set; }
 
         [JsonProperty("voiceControlSettings")] 
-        public VoiceControlSettings VoiceControlSettings { get; set; }
+        public VoiceControlSettings VoiceControlSettings { get; private set; }
 
         [JsonProperty("ruleGroups")] 
-        public List<Guid> RuleGroups { get; set; }
+        public List<Guid> RuleGroups { get; private set; }
 
         [JsonProperty("ruleMetaDatas")]
-        public RuleMetaDatasCollection RuleMetaDatas { get; set; }
+        public RuleMetaDatasCollection RuleMetaDatas { get; private set; }
 
         [JsonProperty("liveOTAUStatus")] 
-        public LiveOTAUStatus LiveOTAUStatus { get; set; }
+        public LiveOTAUStatus LiveOTAUStatus { get; private set; }
 
         [JsonProperty("liveUpdateSupported")] 
-        public bool? LiveUpdateSupported { get; set; }
+        public bool? LiveUpdateSupported { get; private set; }
 
         [JsonProperty("id")] 
-        public string Id { get; set; }
+        public string Id { get; private set; }
 
         [JsonProperty("accessPointUpdateStates")]
-        public AccessPointUpdateStatesDictionary AccessPointUpdateStates { get; set; }
+        public AccessPointUpdateStatesDictionary AccessPointUpdateStates { get; private set; }
 
         [JsonProperty("pinChangeTimestamp", NullValueHandling = NullValueHandling.Ignore)]
         [JsonConverter(typeof(UnixTimestampConverter))]
-        public DateTime PinChangeTimestamp { get; set; }
+        public DateTime PinChangeTimestamp { get; private set; }
 
         [JsonProperty("pinChangeClientLabel")]
-        public string PinChangeClientLabel { get; set; }
+        public string PinChangeClientLabel { get; private set; }
 
     }
 }
